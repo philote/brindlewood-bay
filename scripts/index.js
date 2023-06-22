7eb3701783e1360746516508f1fbb8f7a74e4183
Lavncelot <60672427+Lavncelot@users.noreply.github.com>
2023-06-22 16:31:37 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 import { SCHEMA_EN } from "./schema-en.js";
 import { SCHEMA_RU } from "./schema-ru.js";
 
-Hooks.once("init", () => {
+Hooks.once("pbtaSheetConfig", () => {
   game.settings.set("pbta", "sheetConfigOverride", true);
 
   let schema = game.modules.get("ru-ru")?.active ? SCHEMA_RU : SCHEMA_EN;
